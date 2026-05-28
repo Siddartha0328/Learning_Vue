1af1af27bd62b6461f76e892cad2d90840910895
Siddartha0328 <siddartha@appspiresolutions.in>
2026-05-28 11:59:14 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+const developer = 'Siddu'
+const role = 'Future Vue.js Engineer'
+</script>
+
+<template>
+  <div class="container">
+    <h1>Welcome, {{ developer }}</h1>
+    <p>Today I begin my journey as a {{ role }}.</p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  font-family: system-ui, sans-serif;
+  text-align: center;
+  padding: 4rem;
+}
+h1 {
+  color: #42b883;
+}
+</style>
